2341e0788c00fe36f469448808b74f27da4e69b5
SixtyByte <valentinvodopyan@ukr.net>
2022-12-02 22:05:00 +0200

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+import { Notify } from "notiflix" 
+export default fetchCountries
+
+function fetchCountries(name){const base_url=`http://restcountries.com/v2/name/`
+    return fetch(`${base_url}${name}?fields=name,capital,population,flags,languages`).then(resp=>{if(!resp.ok){
+    throw new Error(Notify.failure(`Oops, there is no country with that name`))
+      }
+      return resp.json()
+}).catch(err=>console.error(err))
+
+}
+fetchCountries()
